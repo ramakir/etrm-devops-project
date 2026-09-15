383b0a6b36c72488e7e507a63d3366739d345bca
Kiran <kiranr.ramanna@gmail.com>
2026-09-15 11:27:34 +0530

    Add isolated CI test database

--- application/trade-service/src/test/java/com/etrm/tradeservice/TradeServiceApplicationTests.java
+++ application/trade-service/src/test/java/com/etrm/tradeservice/TradeServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.etrm.tradeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TradeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
